a64ca59f9e037de74429ef2750a82d2e741c2308
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-11-22 17:49:27 +0100

    Nowy projekt

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Arkadiusz Plumbaum on 22/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Poor/I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by Arkadiusz Plumbaum on 22/11/2024.
+//
+
+import Testing
+@testable import I_Am_Poor
+
+struct I_Am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
