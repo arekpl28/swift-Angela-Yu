065fa9cfda0f11889afecdc69f57f521f4f273c9
Arkadiusz Plumbaum <arekpl28@wp.pl>
2024-11-23 14:44:47 +0100

    dice game

---
+++ DiceeGame/DiceeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceeGame
+//
+//  Created by Arkadiusz Plumbaum on 23/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DiceeGame/DiceeGameTests/DiceeGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeGameTests.swift
+//  DiceeGameTests
+//
+//  Created by Arkadiusz Plumbaum on 23/11/2024.
+//
+
+import Testing
+@testable import DiceeGame
+
+struct DiceeGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
